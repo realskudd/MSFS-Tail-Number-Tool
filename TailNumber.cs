a9bb109ef9de03e6cdf04717416a1dc47de3a6d1
skudd <skudd@skudd.com>
2022-02-26 17:55:43 -0600

    Refs #1 - Added historic records

---
+++ TailNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace MSFS_2020_Tail_Number_Fixer___FSUIPC
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class TailNumber
+    {
+        [JsonProperty]
+        public string Registration { get; set; }
+
+        [JsonProperty]
+        public string AircraftModel { get; set; }
+
+        public TailNumber() { }
+
+        public TailNumber(string registration, string model)
+        {
+            Registration = registration;
+            AircraftModel = model;
+        }
+
+        public override string ToString()
+        {
+            return $"{Registration} - {AircraftModel}";
+        }
+    }
+}
